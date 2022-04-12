a4db5e511326bc897c3a66044ceee698b48b79b5
srnyx <25808801+srnyx@users.noreply.github.com>
2022-04-11 22:01:58 -0400

    Remove unnecessary stuff from `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-group = "PATH"
-version = "0.0.1"
 description = "NAME"
+version = "0.0.1"
+group = "PATH"
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
@@ -13,36 +12,4 @@ repositories {
 
 dependencies {
 //    compileOnly("PATH:NAME:VERSION")
-}
-
-tasks {
-    shadowJar {
-        dependencies {
-//            exclude(dependency("org.spigotmc:spigot-api(:1.18.2-R0.1-SNAPSHOT"))
-        }
-    }
-
-    jar {
-        dependsOn("shadowJar")
-        isEnabled = false
-    }
-
-    // Set UTF-8 as the encoding
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-
-    // Process Placeholders for the plugin.yml
-    processResources {
-        filesMatching("**/plugin.yml") {
-            expand(rootProject.project.properties)
-        }
-        // Always re-run this task
-        outputs.upToDateWhen { false }
-    }
-
-    compileJava {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
-    }
 }
