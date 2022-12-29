d49495c95d20be7ce2e1ba03d5be07d4c420bb3c
srnyx <25808801+srnyx@users.noreply.github.com>
2022-12-28 21:56:02 -0500

    Annoying API implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,44 @@
-description = "name"
 version = "0.0.1"
-group = "tld.domain"
+group = "xyz.srnyx"
 
 plugins {
     java
+    id("com.github.johnrengelman.shadow") version "VERSION" // 7.1.2
 }
 
 repositories {
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // org.spigotmc:spigot-api
     mavenCentral() // org.spigotmc:spigot-api
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots") // org.spigotmc:spigot-api
+    maven("https://jitpack.io") // xyz.srnyx:annoying-api
 }
 
 dependencies {
-    compileOnly("org.spigotmc", "spigot-api", "1.19.2-R0.1-SNAPSHOT")
-    implementation("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.spigotmc", "spigot-api", "1.19.3-R0.1-SNAPSHOT")
+	implementation("xyz.srnyx", "annoying-api", "VERSION") // https://srnyx.xyz/api
+    compileOnly("org.jetbrains:annotations:23.1.0")
 }
 
 tasks {
+    // Make 'gradle build' run 'gradle shadowJar'
+    build {
+        dependsOn("shadowJar")
+    }
+
+    // Remove '-all' from the JAR file name and relocate the AnnoyingAPI package
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("xyz.srnyx.annoyingapi", "xyz.srnyx.PACKAGE.annoyingapi")
+    }
+
+    // Text encoding
     compileJava {
         options.encoding = "UTF-8"
     }
 
+    // Replace '${name}' and '${version}'
     processResources {
-        inputs.property("version", project.version)
         filesMatching("**/plugin.yml") {
-            expand("version" to project.version)
+            expand("name" to project.name, "version" to project.version)
         }
     }
 }
---
+++ src/main/java/xyz/srnyx/NAME/name.java
@@ -0,0 +1,16 @@
+package xyz.srnyx.NAME;
+
+import xyz.srnyx.annoyingapi.AnnoyingPlugin;
+
+
+public class NAME extends AnnoyingPlugin {
+    public NAME() {
+        super();
+
+        // Options
+        //options.KEY = VALUE;
+
+        // Dependencies
+        //options.dependencies.add(AnnoyingDependency);
+    }
+}
