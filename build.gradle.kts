fd3fa232acf6cb75981ab961eeb5873bb4f700f9
srnyx <25808801+srnyx@users.noreply.github.com>
2023-08-05 10:38:33 -0400

    Update Gradle Galaxy & Annoying API

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.0.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-setupAnnoyingAPI("994c10aef2", "xyz.srnyx", "0.0.1", "DESCRIPTION")
+setupAnnoyingAPI("4.1.0", "xyz.srnyx", "0.0.1", "DESCRIPTION")
 spigotAPI("1.8.8")
