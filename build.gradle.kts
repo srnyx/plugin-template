91881b3c5b6f3320fc1afc22c83b0cc8942fbccb
srnyx <25808801+srnyx@users.noreply.github.com>
2024-05-30 08:46:20 -0400

    Update dependencies
    Gradle Galaxy: 1.1.2 -> 1.1.3
    Shadow: 8.1.1 -> 8.1.7
    Annoying API: 8469d47bd7 -> 723fb94dbb
    Gradle: 8.6 -> 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.1.3"
+    id("io.github.goooler.shadow") version "8.1.7"
 }
 
-setupAnnoyingAPI("8469d47bd7", "xyz.srnyx", "0.0.1", "DESCRIPTION")
+setupAnnoyingAPI("723fb94dbb", "xyz.srnyx", "0.0.1", "DESCRIPTION")
 spigotAPI("1.8.8")
